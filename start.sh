4368b3bc9fba4c7fd7db6975ea99824c448998b4
Karl Keppner <karl@realio.fund>
2025-02-19 10:33:56 -0700

    start command

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp /home/callisto/config/config.yaml /home/callisto/.callisto/config.yaml
-callisto parse genesis-file --gensis-file-path /home/callisto/.callisto/genesis.json && callisto start
+callisto parse genesis-file --genesis-file-path /home/callisto/.callisto/genesis.json && callisto start
