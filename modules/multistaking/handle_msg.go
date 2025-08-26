5a5702707b694ae43645365a68da843c9bbd473a
Hieu Vu <hieuvubk@gmail.com>
2025-08-26 15:47:50 +0700

    revert

--- modules/multistaking/handle_msg.go
+++ modules/multistaking/handle_msg.go
@@ -209,6 +209,5 @@ func (m *Module) UpdateUnlockToken(height int64, stakerAddr string, valAddr stri
 			}
 		}
 	}
-	m.db.SaveUnbondingToken2(height, total)
-	return m.db.SaveUnbondingToken(height, []*multistakingtypes.MultiStakingUnlock{unlock})
+	return m.db.SaveUnbondingToken2(height, total)
 }
