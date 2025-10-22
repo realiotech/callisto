551ea779a0e34298d195327959d9bc272655aadd
Hieu Vu <hieuvubk@gmail.com>
2025-10-22 14:29:42 +0700

    refactor

--- modules/distribution/handle_block.go
+++ modules/distribution/handle_block.go
@@ -1,8 +1,6 @@
 package distribution
 
 import (
-	"fmt"
-
 	juno "github.com/forbole/juno/v6/types"
 
 	tmctypes "github.com/cometbft/cometbft/rpc/core/types"
@@ -10,7 +8,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	"github.com/forbole/callisto/v4/types"
 )
 
 // HandleBlock implements BlockModule
@@ -29,27 +26,9 @@ func (m *Module) HandleBlock(
 		if event.Type == "withdraw_rewards" {
 			delegator, _ := juno.FindAttributeByKey(event, stakingtypes.AttributeKeyDelegator)
 			amount, _ := juno.FindAttributeByKey(event, sdk.AttributeKeyAmount)
-			log.Debug().Str("module", "distribution").Int64("height", block.Block.Height).
-				Msg("updating txs by event")
-
-			currentReward, _ := m.db.GetRewardEarnedByDelegator(delegator.Value)
-			rewardCoin, _ := sdk.ParseCoinNormalized(amount.Value)
-
-			if currentReward == nil {
-				reward := types.NewRewardEarned(delegator.Value, rewardCoin, block.Block.Height)
-				fmt.Println("delegator %s first earned %s", delegator.Value, rewardCoin.String())
-				err := m.db.SaveRewardEarned(reward)
-				if err != nil {
-					return err
-				}
-			} else {
-				rewardCoin = rewardCoin.Add(currentReward.Coin)
-				reward := types.NewRewardEarned(delegator.Value, rewardCoin, block.Block.Height)
-				fmt.Println("delegator %s earned %s", delegator.Value, rewardCoin.String())
-				err := m.db.SaveRewardEarned(reward)
-				if err != nil {
-					return err
-				}
+			err := m.updateRewardEarned(delegator.Value, amount.Value, block.Block.Height)
+			if err != nil {
+				return err
 			}
 		}
 	}
