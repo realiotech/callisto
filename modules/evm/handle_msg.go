9512edc09dda24e2b4a1504cf48260459ad06c1d
Hieu Vu <hieuvubk@gmail.com>
2026-01-05 11:14:02 +0700

    no more v2

--- modules/evm/handle_msg.go
+++ modules/evm/handle_msg.go
@@ -3,21 +3,19 @@ package evm
 import (
 	"fmt"
 
-	"github.com/rs/zerolog/log"
 	juno "github.com/forbole/juno/v6/types"
+	"github.com/rs/zerolog/log"
 
 	"github.com/forbole/callisto/v4/utils"
 
 	// evmtypes "github.com/evmos/os/x/evm/types"
 	cosmosevmtypes "github.com/cosmos/evm/x/vm/types"
-	cosmosevmtypesv1 "github.com/cosmos/evm/x/vm/types/v1"
-	"github.com/realiotech/realio-network/crypto/legacytx"
+	"github.com/cosmos/evm/x/vm/types/legacy"
 )
 
 var msgFilter = map[string]bool{
-	"/os.evm.v1.MsgEthereumTx":  true,
+	"/os.evm.v1.MsgEthereumTx":        true,
 	"/cosmos.evm.vm.v1.MsgEthereumTx": true,
-	"/cosmos.evm.vm.v2.MsgEthereumTx": true,
 }
 
 // HandleMsgExec implements modules.AuthzMessageModule
@@ -35,17 +33,13 @@ func (m *Module) HandleMsg(_ int, msg juno.Message, tx *juno.Transaction) error 
 
 	switch msg.GetType() {
 	case "/os.evm.v1.MsgEthereumTx":
-		cosmosMsg := utils.UnpackMessage(m.cdc, msg.GetBytes(), &legacytx.MsgEthereumTx{})
+		cosmosMsg := utils.UnpackMessage(m.cdc, msg.GetBytes(), &legacy.MsgEthereumTx{})
 		return m.db.SaveEvmTx(int64(tx.Height), tx.TxHash, cosmosMsg.Hash)
 	// evm v0.2.0 msgs
 	case "/cosmos.evm.vm.v1.MsgEthereumTx":
-		cosmosMsg := utils.UnpackMessage(m.cdc, msg.GetBytes(), &cosmosevmtypesv1.MsgEthereumTx{})
-		return m.db.SaveEvmTx(int64(tx.Height), tx.TxHash, cosmosMsg.Hash)
-	// evm v0.5.0 msgs
-	case "/cosmos.evm.vm.v2.MsgEthereumTx":
 		cosmosMsg := utils.UnpackMessage(m.cdc, msg.GetBytes(), &cosmosevmtypes.MsgEthereumTx{})
-		return m.db.SaveEvmTx(int64(tx.Height), tx.TxHash, cosmosMsg.Raw.Hash().String())
+		return m.db.SaveEvmTx(int64(tx.Height), tx.TxHash, cosmosMsg.GetHash().String())
 	}
-	
+
 	return nil
 }
